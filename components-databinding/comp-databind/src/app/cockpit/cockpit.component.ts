30fae3baa76c80a706a1d12cbab06962ce15b5e5
janani09 <jananimurthi09@gmail.com>
2023-02-23 02:23:21 +0530

    viewchild-to fetch the data from html to ts with local reference

--- components-databinding/comp-databind/src/app/cockpit/cockpit.component.ts
+++ components-databinding/comp-databind/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -8,15 +8,21 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class CockpitComponent {
   @Output() serverCreated=new EventEmitter<{serverName:string,serverContent:string}>();
   @Output() bluePrintCreated=new EventEmitter<{serverName:string,serverContent:string}>();
-  newserverName='';
-  newserverContent='';
+  @ViewChild('serverNameContent',{static:true}) serverContent;
 
-  onAddserver(nameInput:HTMLInputElement,nameContent:HTMLInputElement){
+  ngOnInit(){
+    console.log(this.serverContent);//returns elementRef
+    
+  }
+
+  onAddserver(nameInput:HTMLInputElement,nameContent:HTMLInputElement): void{
+    console.log("Adding native Element value" +this.serverContent.nativeElement.value);//to get the native element
     this.serverCreated.emit({serverName:nameInput.value,serverContent:nameContent.value});
 
   }
   onAddBluePrint(nameInput:HTMLInputElement,nameContent:HTMLInputElement){
-    console.log(nameInput.value)
+   // console.log(nameInput.value)
+   this.serverContent.nativeElement.value="setting Random value"; // to set the random value
     this.bluePrintCreated.emit({serverName:nameInput.value,serverContent:nameContent.value});
   }
 
